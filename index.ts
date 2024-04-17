29edb94289424eb7956f6eb5799a5df9827548cc
ujk-droid <03312436713aa@gmail.com>
2024-04-17 05:51:21 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+
+import { log } from "console";
+import inquirer from "inquirer";
+
+const answers:{
+    sentence:string
+
+} = await inquirer.prompt([
+    {
+        name: "sentence",
+        type:"input",
+        message:"Enter your sentence to count to the word: "
+}
+])
+const words=answers.sentence.trim().split(" ")
+
+console.log(words);
+console.log(`your sentence word count is ${words.length} `);
